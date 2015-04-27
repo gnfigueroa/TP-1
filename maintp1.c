9be3a9b3ebc42fa5577e696f3ddbd63da4ae64d8
gnfigueroa <gabrieln.figueroa@yahoo.com>
2015-04-27 00:39:43 -0300

    modificacion ubicacion del include

--- maintp1.c
+++ maintp1.c
@@ -1,20 +1,24 @@
-#include <stdio.h>
-
 /*
 Programa pasaje de Celsius a Fahreinheit (0 a 300)
 Gabriel Nicolas Figueroa
 26 de abril del 2015 
 */
+
+#include <stdio.h>
+
 int main(void)
 {
-
  float fahr, celsius;
  float lower, upper, step;
+
  lower = 0; /* lower limit of temperatuire scale */
  upper = 300; /* upper limit */
  step = 20; /* step size */
+
  celsius = lower;
+
  printf("Celsius      Fahrenheit\n");
+
  while (celsius <= upper) {
        fahr = (9.0/5.0 * celsius)+32.0;
        printf("%6.1f        %3.1f \n", celsius,fahr);
